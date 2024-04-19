2e8b534063ccab7a352b28d96660cf066134fd02
JohnStewardson <jjf.stewardson@gmail.com>
2024-04-19 22:25:45 +0200

    This shows that the motion df is indeed correct, just the first rows contain a lot of NaN (only in the 70th row are all values present)

--- creating_dataframes.py
+++ creating_dataframes.py
@@ -9,4 +9,5 @@ df_location_1 = create_combined_dataframe(user1_folder_path)
 df_motion_1 = create_combined_dataframe_motion(user1_folder_path)
 df_label_1 = create_combined_dataframe_label(user1_folder_path, activity_mapping)
 
-
+df_motion_1_filled = df_motion_1.dropna(how='any')
+print(df_motion_1_filled.head())
